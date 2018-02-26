d871af6a58dce893bc2aa45c4230402f127ab12d
Milena Tomova <kokolina1888@gmail.com>
2018-02-26 14:56:49 +0200

    sync

--- 11_bootstrap/grid.php
+++ 11_bootstrap/grid.php
@@ -4,9 +4,21 @@
 		<meta charset="UTF-8">
 		<title>Document</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</head>
 	<body>
 		<div class="container">
+		<ul class="nav nav-tabs">  
+  			<li role="presentation" class="dropdown">
+    			<a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+      				Dropdown <span class="caret"></span>
+    			</a>
+    			<ul class="dropdown-menu">
+      				Dropdown
+    			</ul>
+  			</li>  
+		</ul>
 			<div class="row">
 				<div class="col-md-2 col-md-offset-2">
 					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
