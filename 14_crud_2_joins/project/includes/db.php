15bf9512f01138b99d0cde5806b03c4baf03e053
Milena Tomova <kokolina1888@gmail.com>
2018-03-04 12:01:34 +0200

    sync

---
+++ 14_crud_2_joins/project/includes/db.php
@@ -0,0 +1,10 @@
+<?php 
+
+// $conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect('localhost', 'root', '', 'vso_recepices');
+
+// if (!$conn) {
+// 	die("Connection failed: " . mysqli_connect_error());
+// } else {
+// 	echo "Connected successfully !";
+// }
---
+++ 14_crud_2_joins/project/includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>CRUD and JOINS</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<h1>The Header</h1>
+<!-- end header -->
